ab856066094324a8793d0fea775313ee64043b2e
HEun0420 <usialeta@gmail.com>
2024-06-11 17:37:15 +0900

    Subqueries

--- 07_GROUPING.sql
+++ 07_GROUPING.sql
@@ -41,3 +41,130 @@ from
     tbl_menu
 group by
     category_code;
+
+-- category_code, orderable_status -> 그룹
+-- 총갯수, 가격 평균, 가격 총합 -> 별칭도 적용
+select
+    category_code,
+    orderable_status,
+    count(*) 총갯수,
+    sum(menu_price) 총합,
+    avg(menu_price) 평균
+from
+    tbl_menu
+group by
+    category_code,
+    orderable_status
+order by
+    orderable_status;
+
+/*
+HAVING
+- GROUP BY 와 함께 사용되며, 그룹화된 결과에 조건을 적용할 때 사용된다.
+- WHERE 는 개별 행에 조건을 적용하는 것과 달리
+- HAVING 절은 그룹화된 데이터에 조건을 적용한다.
+*/
+-- menu_price, category_code로 그루핑된 데이터에서
+-- category_code가 5 이상, 8이하 만 조회
+select
+    menu_price,
+    category_code
+from
+    tbl_menu
+group by
+    menu_price,
+    category_code
+having
+    category_code >= 5 and category_code <= 8
+order by
+    menu_price;
+
+-- where 절로도 할 수 있다.
+-- 그룹화 전에 필터링을 할 수 없는 경우에는 HAVING으로만 가능하다.
+select
+    menu_price,
+    category_code
+from
+    tbl_menu
+where
+    category_code >= 5 and category_code <= 8
+group by
+    menu_price,
+    category_code
+order by
+    menu_price;
+
+-- 카테고리 코드 그룹 별 평균 가격을 구하고, 평균 가격이 8000원 이상인 경우만 조회
+select
+    category_code,
+    avg(menu_price)
+from
+    tbl_menu
+group by
+    category_code
+having
+    avg(menu_price) >= 8000;
+
+/*
+WITH ROLLUP
+- GROUP BY에서 사용되는 확장기능으로 다양한 수준의 집계 데이터를 생성 할 수 있다.
+*/
+-- 총합을 구할때
+select
+    category_code,
+    sum(menu_price)
+from
+    tbl_menu
+group by
+    category_code
+with rollup;
+
+-- 컬럼 두개를 활용한 WITH ROLLUP
+-- 메뉴 가격별 총합 및 해당 메뉴 가격별 같은 카테고리의 총합
+select
+    menu_price,
+    category_code,
+    count(*),
+    sum(menu_price)
+from
+    tbl_menu
+group by
+    menu_price,
+    category_code
+with rollup;
+
+
+-- 1. 특정 카테고리 총 메뉴 가격이 20000원 이상인 경우
+select
+    category_code,
+    sum(menu_price)
+from
+    tbl_menu
+group by
+    category_code
+having
+    sum(menu_price) >= 20000;
+
+-- 2. 특정 카테고리의 메뉴 수가 3개 이상인 경우
+select
+    category_code,
+    count(menu_code)
+from
+    tbl_menu
+group by
+    category_code
+having
+    count(menu_code) >= 3;
+
+-- 3. 특정 카테고리에서 주문 가능한 메뉴의 평균 가격이 10000원 이상인경우
+select
+    category_code,
+    avg(menu_price)
+from
+    tbl_menu
+where
+    orderable_status = 'Y' -- 주문가능한 메뉴
+group by
+    category_code
+having
+    avg(menu_price) > 10000;
--- 08_SUBQUERIES.sql
+++ 08_SUBQUERIES.sql
@@ -1,31 +1,32 @@
--- Subquery
--- 서브 쿼리는 다른 쿼리에서 실행되는 쿼리
+-- SUBQUERY
+-- 서브쿼리는 다른 쿼리에서 실행되는 쿼리
 -- 존재하지 않는 조건에 근거한 값들을 검색하고자 할 때 사용한다.
 -- 메인쿼리가 서브쿼리를 포함하는 종속적인 관계이다.
 
 -- 서브쿼리의 유형
 -- 1. 일반 서브쿼리
---   1-a. 단일행 일반 서브쿼리
---   1-b. 다중행 일반 서브쿼리
+--  1-a. 단일행 일반 서브쿼리
+--  1-b. 다중행 일반 서브쿼리
 -- 2. 상관 서브쿼리
---   2-a. 스칼라 서브쿼리
+--  2-a. 스칼라 서브쿼리
 
 -- 서브쿼리의 규칙
--- 서브쿼리는 반드시 소괄호()로 묶어야한다. (select...) gudxo
--- 서브쿼리는 연산자의 오른쪽에 위치해야한다.
--- 서브쿼리 내에서 order by 문법은 지원되지 안흔다.
+-- 서브쿼리는 반드시 소괄호()로 묶어야한다. (SELECT ...) 형태
+-- 서브쿼리는 연산자의 오른쪽에 위치 해야한다.
+-- 서브쿼리 내에서 ORDER BY 문법은 지원되지 않는다.
 
--- 단일행 서브쿼리
--- 민트미역국이랑 같은 카테고리의 메뉴 조회
--- (메인코드, 메뉴명, 가격, 주문가능여부)
+-- 단일 행 서브쿼리
+-- 민트미역국 이랑 같은 카테고리의 메뉴 조회
+-- (메뉴코드, 메뉴명, 가격, 주문가능여부)
 
-/*쿼리 2번을 이용해서 원하는 값 구하기 */
+/* 쿼리 2번 사용해서 원하는 결과 구하기 */
+-- 서브쿼리
 select
     category_code
 from
     tbl_menu
 where
-    menu_name = '민트미역국';  --  4
+    menu_name = '민트미역국'; -- 4
 
 select
     menu_code 메뉴코드,
@@ -37,27 +38,32 @@ from
 where
     category_code = 4;
 
-    /*서브쿼리를 이용해서 값으 구함*/
- select
-     menu_code 메뉴코드,
-     menu_name 메뉴명,
-     menu_price 메뉴가격,
-     orderable_status 주문가능여부
- from
-     tbl_menu
- where
-     category_code = (-- 괄호 안에 쿼리문을 넣어서 서브쿼리로 사용 가능하다.
-         select category_code
-         from tbl_menu
-         where menu_name = '민트미역국');
+/* 서브쿼리를 사용해 구하기 */
+select
+    menu_code 메뉴코드,
+    menu_name 메뉴명,
+    menu_price 메뉴가격,
+    orderable_status 주문가능여부
+from
+    tbl_menu
+where
+    category_code = ( -- 괄호 안에 쿼리문을 넣어서 서브쿼리로 사용 가능하다.
+        select
+            category_code
+        from
+            tbl_menu
+        where
+            menu_name = '민트미역국'
+    );
 
 -- 다중행 서브쿼리
--- 카테고리 코드가 식사인 모든 category_code를 조회
+-- 카테고리 코드가 식사인 모든 category_code 를 조회
 select
     category_code
 from
-    tbl_menu;
-
+    tbl_category
+where
+    ref_category_code = 1;
 
 select
     *
@@ -71,18 +77,18 @@ where
             tbl_category
         where
             ref_category_code = 1
-        );
+    );
 
 -- all 연산자
--- 서브쿼리의 결과 모두에 대한 연산결과가 참이면 참을 반환한다.
--- x > All(..) : 모든 값보다 크면 참, 최대값보다도 크면 참
--- x >= All(..) : 모든 값보다 크거나 같으면 참, 최대값보다도 크거나 같으면 참
--- x < All(..) : 모든 값보다 작으면 참, 최소값보다도 작으면 참
--- x <= All(..) : 모든 값보다 작거나 같으면 참, 최소값보다도 작거나 같으면 참
--- x = All(..) : 모든 값과 같으면 참
--- x != All(..) : 모든 값과 다르면 참 (NOt In과 동일하다.)
+-- 서브쿼리의 결과 모두에 대해 연산결과가 참이면 참을 반환한다.
+-- x > ALL(..) : 모든 값보다 크면 참, 최대값보다도 크면 참
+-- x >= ALL(..) : 모든 값보다 크거나 같으면 참, 최대값보다 크거나 같으면 참
+-- x < ALL(..) : 모든 값보다 작으면 참, 최소값보다도 작으면 참
+-- x <= ALL(..) : 모든 값보다 작거나 같으면 참, 최소값보다 작거나 같으면 참
+-- x = ALL(..) : 모든 값과 같으면 참
+-- x != ALL(..) : 모든 값과 다르면 참 (NOT IN이랑 동일하다.)
 
--- 가장 비싼 메뉴 조회
+-- 가장 비싼 메뉴를 조회
 select
     max(menu_price)
 from
@@ -93,38 +99,42 @@ select
 from
     tbl_menu
 where
-    menu_price /* 22000*/ >= all(
-        select -- tbl_menu에 있는 가격들
-            menu_price
+    menu_price/*22000*/ >= all(
+        select -- tbl_menu에 있는 메뉴 가격들
+               menu_price
         from
             tbl_menu
     );
 
--- 한식보다 비싼 양식  or 중식을 찾아붜
+-- 한식보다 비싼 일식 or 중식을 찾아줘
 select
     *
 from
     tbl_menu
 where
-    category_code in(5,6) -- 일식/중식만 조회
-    and
+    category_code in (5,6) -- 일식/ 중식만 조회
+
+  and
+
     menu_price > all( -- 한식의 메뉴 가격들
         select
             menu_price
         from
             tbl_menu
-        where category_code =4 -- 한식 코드
+        where category_code = 4
     );
+
 
 -- 상관 서브쿼리
 -- 메인 쿼리가 서브쿼리의 결과에 영향을 주는 경우를 의미한다.
 -- 메인쿼리의 값을 서브쿼리에게 주고 서브쿼리를 수행한 다음
--- 그 결과를 다시 메인쿼리로 반환하는 형식으로 수앻되는 서브쿼리
+-- 그결과를 다시 메인쿼리로 반환하는 형식으로 수행되는 서브쿼리
 
--- 서브쿼리의 where절 수행을 위해, 메인쿼리가 먼저 수행되는 구조
+-- 서브쿼리의 WHERE 절 수행을 위해, 메인쿼리가 먼저 수행되는 구조
 -- 메인쿼리 테이블의 행에 따라 서브쿼리의 결과값도 바뀐다.
 
--- 카테고리별 평균 가격이 비싼 메뉴 조회
+
+-- 카테고리 별 평균 가격이 가장 비싼 메뉴 조회
 select
     category_code,
     max(menu_price)
@@ -138,18 +148,19 @@ select
 from
     tbl_menu a -- 이 테이블을
 where
-    menu_price =(
+    menu_price = (
         select
             max(menu_price)
         from
             tbl_menu
         where category_code = a.category_code -- 여기서 가져다 쓴다.
-        );
+    );
 
 -- EXISTS
 -- 서브쿼리 결과집합에 행이 존재하면 참, 행이 존재하지 않으면 거짓
--- Not Exists
+-- NOT EXISTS
 -- 서브쿼리 결과집합에 행이 존재하지 않으면 참, 행이 존재하면 거짓
+
 
 -- tbl_menu에서 사용되는 카테고리만 조회하겠다.
 select
@@ -157,17 +168,17 @@ select
 from
     tbl_category a
 where
-    exists(
+    not exists(
         select
             category_code
         from
             tbl_menu b
         where
-            b.category_code= a.category_code
+            b.category_code = a.category_code
     );
 
 -- 스칼라 서브쿼리
--- 결과값이 1개인 서브쿼리, 주로 select문에서 사용된다,
+-- 결과값이 1개인 서브쿼리, 주로 select 문에서 사용된다.
 -- 스칼라 = 단일값
 
 -- 메뉴명, 카테고리명 조회
@@ -176,9 +187,10 @@ select
     b.category_name
 from
     tbl_menu a
-left join tbl_category b
-on a.category_code = b.category_code;
+        left join tbl_category b
+                  on a.category_code = b.category_code;
 
+-- 스칼라 서브쿼리
 select
     a.menu_name,
     (select
@@ -186,7 +198,7 @@ select
      from
          tbl_category b
      where b.category_code = a.category_code
-     ) category_name
+    ) category_name
 from
     tbl_menu a;
 
@@ -194,29 +206,30 @@ from
 select
     menu_name,
     (select
-        category_name
-    from
-        tbl_category)category_name
+         category_name
+     from
+         tbl_category) category_name
 from
     tbl_menu;
 
 /*
- CTE(common table expressions)
- - 서브쿼리랑 비슷한 개념
- - 코드의 가독성과 재사용성을 위해 사용한다.
- - from 절에서만 사용이된다. (join 일시 join 구문에서도 가능)
- */
+CTE(Common Table Expressions)
+- 서브쿼리랑 비슷한 개념
+- 코드의 가독성과 재사용성을 위해 사용한다.
+- FROM 절에서만 사용이된다. (JOIN 일시 JOIN 구문에서도 가능)
+*/
 
- -- 메뉴명과 카테고리명을 함께 출력
-with menucate as(
+-- 메뉴명과 카테고리명을 함께 출력
+with menucate as (
     select
         menu_name,
         category_name
     from
         tbl_menu a
-    join
-        tbl_category b on a. category_code =  b.category_code
+            join
+        tbl_category b on a.category_code = b.category_code
 )
+
 select
     *
 from
