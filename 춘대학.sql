537c14ae610e1df59613435e3e7cb781cb5a4df8
HEun0420 <usialeta@gmail.com>
2024-06-13 15:23:53 +0900

    춘대학

--- 춘대학.sql
+++ 춘대학.sql
@@ -4,3 +4,12 @@ select * from TB_PROFESSOR; -- 교수테이블
 select * from TB_CLASS; -- 수업테이블
 select * from TB_CLASS_PROFESSOR; -- 수업교수테이블
 select * from TB_GRADE; -- 학점테이블
+
+-- 1. 춘 기술대학교의 학과 이름과 계열을 표시하시오.
+-- 단, 출력 헤더는 "학과 명", "계열"으로 표시하도록 한다.
+select
+    as 학과명,
+    as 계열
+from tb_deparment
+
+
